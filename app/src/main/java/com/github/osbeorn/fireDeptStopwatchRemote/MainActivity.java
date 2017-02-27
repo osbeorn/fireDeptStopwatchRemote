f93bc645babdbd9f59bed3279365b17d3b6f405a
Benjamin Kastelic <kastelic.benjamin@gmail.com>
2017-02-27 22:00:15 +0100

    added the rest of the files

---
+++ app/src/main/java/com/github/osbeorn/fireDeptStopwatchRemote/MainActivity.java
@@ -0,0 +1,23 @@
+package com.github.osbeorn.fireDeptStopwatchRemote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
